30cbec6e00e37c8dacc4b75bae7054e2c04da0bf
edsinkevic <sinkevicedwin@gmail.com>
2022-12-02 18:46:44 +0200

    Solve first part, not cleaned

--- 2/main.asm
+++ 2/main.asm
@@ -1,17 +1,34 @@
 section .bss
-buffer: resb 4 ; A 2 KB byte buffer used for read
-fd: resb 0
+buffer: resb 5 ; A 2 KB byte buffer used for read
+fd: resb 1
 
 section .data
-buflen: dw 4 ; Size of our buffer to be used for read
+buflen: dd 4 ; Size of our buffer to be used for read
+score:  dd 0
+dalbajob: db 'dalbajob', 0
 
+firstRockCode:  db 'A'
+firstPaperCode: db 'B'
+firstScissorCode: db 'C'
+
+secondRockCode: db 'X'
+secondPaperCode:  db 'Y'
+secondScissorCode:  db 'Z'
+rock: db 1
+paper:  db 2
+scissor:  db 3
+lineLength:  db 4
+roundScore:  db 0
+winScore:  db 6
+drawScore: db 3
 
 section .text
 global _start
-_start:
-; open(char *path, int flags, mode_t mode);
+extern intToString, atoi, iprintLF, sprint, openFileByName
 
-; Get our command line arguments.
+_start:
+  ; open(char *path, int flags, mode_t mode);
+mov byte [buflen + 4], 0
 pop ebx ; argc
 pop ebx ; argv[0] (executable name)
 pop ebx ; argv[1] (desired file name)
@@ -21,17 +38,20 @@ xor edx, edx ; Mode is ignored when O_CREAT isn't specified
 int 0x80 ; Call the kernel
 mov [fd], eax
 test eax, eax ; Check the output of open()
-jns file_read ; If the sign flag is set (positive) we can begin reading the file
+jns fileRead ; If the sign flag is set (positive) we can begin reading the file
 
 ; = If the output is negative, then open failed. So we should exit
 exit:
+mov eax, [score]
+call iprintLF
+
 mov eax, 0x01 ; 0x01 = syscall for exit
 xor ebx, ebx ; makes ebx technically set to zero
 int 0x80
 
 ; = Begin reading the file
 
-file_read:
+fileRead:
 ; read(int fd, void *buf, size_t count);
 mov ebx, [fd] ; Move our file descriptor into ebx
 mov eax, 0x03 ; syscall for read = 3
@@ -41,13 +61,83 @@ int 0x80
 cmp eax, 0
 je exit
 test eax, eax ; Check for errors / EOF
-jz file_out ; If EOF, then write our buffer out.
+jz fileOut ; If EOF, then write our buffer out.
 
-file_out:
+fileOut:
 ; write(int fd, void *buf, size_t count);
-mov edx, eax ; read returns amount of bytes read
-mov eax, 0x04 ; syscall write = 4
-mov ebx, 0x01 ; STDOUT = 1
-mov ecx, buffer ; Move our buffer into the arguments
-int 0x80
-jmp file_read ; All done 
+
+call roundResult
+
+mov ah, 0
+add [score], eax
+call iprintLF
+
+jmp fileRead ; All done
+
+roundResult:
+  mov ch, [buffer + 2]             ; first
+  mov cl, [buffer]         ; second
+
+  mov al, 0
+
+  cmp ch, [secondRockCode]
+  je rockCase
+
+  cmp ch, [secondPaperCode]
+  je paperCase
+
+  cmp ch, [secondScissorCode]
+  je scissorCase
+
+  jmp recover
+
+rockCase:
+  mov al, [rock]
+  cmp cl, [firstScissorCode]
+  je winCase
+  cmp cl, [firstPaperCode]
+  je loseCase
+  jmp drawCase
+
+paperCase:
+  mov al, [paper]
+  cmp cl, [firstRockCode]
+  je winCase
+  cmp cl, [firstScissorCode]
+  je loseCase
+  jmp drawCase
+
+scissorCase:
+  mov al, [scissor]
+  cmp cl, [firstPaperCode]
+  je winCase
+  cmp cl, [firstRockCode]
+  je loseCase
+
+  jmp drawCase
+
+winCase:
+  add al, [winScore]
+  add al, [roundScore]
+  jmp recover
+
+
+loseCase:
+  add al, [roundScore]
+  jmp recover
+
+
+drawCase:
+  add al, [drawScore]
+  add al, [roundScore]
+  jmp recover
+
+recover:
+  ret
+
+pr:
+  push eax
+  mov eax, dalbajob
+  call sprint
+  pop eax
+  ret
